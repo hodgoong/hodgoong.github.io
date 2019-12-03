e5baedf3657b23ec4929cf1230bea95153543742
hodgoong <hodgoong@gmail.com>
2019-12-04 07:56:28 +0900

    working prototype

--- script.js
+++ script.js
@@ -13,23 +13,10 @@ function loadMDs(file){
         if (txtFile.readyState === 4) { // Makes sure the document is ready to parse.
             if (txtFile.status === 200) { // Makes sure the file exists.
                 allText = txtFile.responseText;
-                //lines = txtFile.responseText.split("\n"); // Will separate each line into an array
-                //var customTextElement2 = document.getElementById('f0');
-                //customTextElement2.innerHTML = txtFile.responseText;
-
-	            var reader = new commonmark.Parser();
-                var writer = new commonmark.HtmlRenderer();
-                var parsed = reader.parse(txtFile.responseText); // parsed is a 'Node' tree
-                // transform parsed if you like...
-                var result = writer.render(parsed); // result is a String
-
-                // hypeDocument.getElementById(box).innerHTML = result
-
-                return result;
+                let html = export_html(txtFile.responseText);
+                console.log(html);
+                document.getElementById('mainView').innerHTML = html;
             }
         }
     }
 }
-
-let result = export_html(loadMDs("content.md"));
-console.log(result);
