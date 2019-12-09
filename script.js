26773dc6a629dc0bcc0e3995fbc30ab0fbfe2960
hodgoong <hodgoong@gmail.com>
2019-12-09 20:25:24 +0900

    refined the loadMarkdown function

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-function export_html(data) {
+function exportHtml(data) {
     let converter = new showdown.Converter();
     let html = converter.makeHtml(data);
 
     return html;
 }
 
-function loadMDs(file){
-	var txtFile = new XMLHttpRequest();
-    txtFile.open("GET", "https://hodgoong.github.io/" + file , true);
-    txtFile.send(null);
-    txtFile.onreadystatechange = function() {
-        if (txtFile.readyState === 4) { // Makes sure the document is ready to parse.
-            if (txtFile.status === 200) { // Makes sure the file exists.
-                allText = txtFile.responseText;
-                let html = export_html(txtFile.responseText);
-                console.log(html);
-                document.getElementById('mainView').innerHTML = html;
+function loadMarkdown(fileLoc, divId){
+	var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://hodgoong.github.io/" + fileLoc , true);
+    xhr.send(null);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                let html = exportHtml(xhr.responseText);
+                document.getElementById(divId).innerHTML = html;
             }
         }
     }
