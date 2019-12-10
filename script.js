3a5c181ec748257772a17932f1917af0f10729c4
hodgoong <hodgoong@gmail.com>
2019-12-10 12:07:02 +0900

    folders and html structure added

--- script.js
+++ script.js
@@ -6,8 +6,23 @@ function exportHtml(data) {
 }
 
 function loadMarkdown(fileLoc, divId){
-	var xhr = new XMLHttpRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open("GET", "https://hodgoong.github.io/" + fileLoc , true);
+    xhr.send(null);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                let html = exportHtml(xhr.responseText);
+                document.getElementById(divId).innerHTML = html;
+            }
+        }
+    }
+}
+
+function loadFolder(folderLoc, divId){
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://hodgoong.github.io/" + folderLoc , true);
     xhr.send(null);
 
     xhr.onreadystatechange = function() {
