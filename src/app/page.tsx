c15059cf2dd3fbdfc822e98c5b9fe57376f6ef56
Hojoong Chung <hodgoong@Hojoongui-MacBookPro.local>
2024-07-17 23:46:13 +0900

    landingpage message modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,23 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>V2 site in preparation. Stay tuned. -Hojoong</p>
-    </main>
+      <p>
+
+      </p>Hey, thanks for the visit.
+      <br></br>
+      My site is in renewal.
+      <br></br>
+      If you need to reach me out, please send email to hodgoong@gmail.com
+      <br></br>
+      <br></br>
+      Regards
+      <br></br>
+      <br></br>
+      Hojoong
+      <br></br>
+      <a href="https://hojoongchung.com">
+        <small>https://hojoongchung.com</small>
+      </a>
+    </main >
   );
 }
